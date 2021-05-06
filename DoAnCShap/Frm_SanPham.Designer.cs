ddef54ac38cfeef0f63f04480bdb665463a265b0
nguyenkhanh2003 <74747143+nguyenkhanh2003@users.noreply.github.com>
2021-05-07 06:58:45 +0700

    update  all

--- DoAnCShap/Frm_SanPham.Designer.cs
+++ DoAnCShap/Frm_SanPham.Designer.cs
@@ -104,7 +104,7 @@ namespace DoAnCShap
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(6, 29);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(143, 135);
+            this.pictureBox1.Size = new System.Drawing.Size(160, 148);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
